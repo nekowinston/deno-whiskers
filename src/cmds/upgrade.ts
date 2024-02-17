6022b436902a35be4ee0d366028a0b29b7ce56f0
winston <hey@winston.sh>
2024-02-17 16:52:56 +0100

    fix: upgrade cmd

--- src/cmds/upgrade.ts
+++ src/cmds/upgrade.ts
@@ -4,7 +4,8 @@ import {
 } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/upgrade/mod.ts";
 
 export const upgradeCmd = new UpgradeCommand({
-  provider: [
-    new GithubProvider({ repository: "nekowinston/deno-whiskers" }),
-  ],
+  main: "main.ts",
+  // TODO: refine this down to minimum required permissions
+  args: ["-A"],
+  provider: new GithubProvider({ repository: "nekowinston/deno-whiskers" }),
 });
